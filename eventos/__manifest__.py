f5e46d9880428e765951c90da8b58d6d47432c6f
gacarfaria <gacarfaria@gmail.com>
2017-10-15 09:29:38 -0200

    [ADD] Módulo de Comunicação

---
+++ eventos/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Gabriel Cardoso de Faria
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Eventos',
+    'description': """
+        Gerência de Eventos""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Gabriel Cardoso de Faria',
+    'depends': [
+        'report_py3o',
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        
+        'report/report_atracao.xml',
+        
+        'views/evento.xml',
+        'views/atracao.xml',
+        'views/artista.xml',
+        'views/responsavel.xml',
+    ],
+    'demo': [
+    ],
+}
